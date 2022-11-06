369d90be77cf9d9901b3bf46470f8671e6420df9
radovanbotik <radovanbotik@gmail.com>
2022-11-06 15:39:14 +0100

    ''

--- router/src/pages/Dashboard.jsx
+++ router/src/pages/Dashboard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+export default function Dashboard(props) {
+  const { name, email } = props.user;
+  return (
+    <div>
+      <h4>
+        hello {name} from {email}
+      </h4>
+    </div>
+  );
 }
--- router/src/pages/Login.jsx
+++ router/src/pages/Login.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function Login(props) {
+  const { setUser } = props;
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+
   console.log({ name, email });
+
+  const navigate = useNavigate();
+
   const handleSubmit = e => {
     e.preventDefault();
+    if (!name || !email) return;
+    setUser({ name: name, email: email });
+    navigate("/dashboard");
   };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form className="submit-form" onSubmit={handleSubmit}>
         <h5>login section</h5>
-        <label htmlFor="user-name"></label>
+        <label htmlFor="user-name">your name</label>
         <input
           type="text"
           id="user-name"
           onChange={e => setName(e.target.value)}
           value={name}
         />
-        <label htmlFor="user-email"></label>
+        <label htmlFor="user-email">your email</label>
         <input
           type="text"
           id="user-email"
